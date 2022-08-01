5fe0832aecd52d32d0cadbb00d3a0f768898cc40
Serhii Solianenko <sergeysolyanenko@gmail.com>
2022-08-01 11:20:27 +0300

    delete route , delete unused props

--- src/components/CustomTemplate/CustomTemplate.jsx
+++ src/components/CustomTemplate/CustomTemplate.jsx
@@ -17,15 +17,17 @@ export class CustomTemplate extends React.Component {
         return (
             <div className="wrapper">
                 <div className={s.tamplate}>
-                    <header className="header">
+                    <h1 className="header">
                         {firstName} {lastName}
-                    </header>
-                    <h4>Дата рождения: {date} </h4>
-                    <h4>Номер телефона: {phone} </h4>
-                    <h4>Сайт: {link} </h4>
-                    <h4>О себе: {about} </h4>
-                    <h4>Стек технологий: {stack} </h4>
-                    <h4>Описание последнего проекта: {project} </h4>
+                    </h1>
+                    <ul>
+                        <li>Дата рождения: <br/> {date}</li>
+                        <li>Номер телефона: <br/>{phone}</li>
+                        <li>Сайт:<br/> {link}</li>
+                        <li>О себе:<br/> {about}</li>
+                        <li>Стек технологий:<br/> {stack}</li>
+                        <li>Описание последнего проекта:<br/> {project}</li>
+                    </ul>
                 </div>
                 <div className={s.btnWrap}>
                     <button type="back" onClick={backToForm}>
