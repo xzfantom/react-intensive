a1a508257fe44741afcaa9343b139c3ec6141e45
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2022-08-03 16:23:03 +0300

    Fixed About Us Page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,5 +1,6 @@
 import Header from '../../components/Header/Header'
 import AboutUsPage from '../../pages/AboutUsPage/AboutUsPage';
+import NotFoundPage from '../../pages/NotFoundPage/NotFoundPage';
 import s from './App.module.css';
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <div className={s.wrap}>
       <Header />
       <AboutUsPage />
+      {/* <NotFoundPage /> */}
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Container from './components/common/Container/Container';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Container>
+      <App />
+    </Container>
   </React.StrictMode>
 );
 
