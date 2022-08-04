6d543f3b8829209d175c8c84a1278872362287b4
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2022-08-04 13:28:23 +0300

    Added function to Go Back button

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,13 +1,21 @@
 import BigButton from '../../components/common/BigButton/BigButton'
+import { useHistory, useLocation } from 'react-router-dom'
 import s from './NotFoundPage.module.css'
 import image from '../../components/images/frame.png'
 
 const NotFoundPage = () => {
+  const history = useHistory()
+  const location = useLocation()
+
+  const onGoBack = () => {
+    history.push(location?.state?.from ?? '/')
+  }
+
   return (
     <>
       <img src={image} className={s.image} alt='Not found page' />
       <h1 className={s.message}>404 - Not Found Page</h1>
-      <BigButton text='go home' />
+      <BigButton text='go home' onClick={onGoBack} />
     </>
   )
 }
