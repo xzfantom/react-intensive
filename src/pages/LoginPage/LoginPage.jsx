ff905609f266e186401bc4a00278c3141daec906
Kateryna Borysenko <k.borysenko.kyiv@gmail.com>
2022-08-04 16:08:40 +0300

    Added useRef to log in form

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,52 +1,46 @@
-import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import BigButton from '../../components/common/BigButton/BigButton'
 import s from './LoginPage.module.css'
 //Todo: сделать фокус в инпуте
 
 const LoginPage = () => {
-  const [name, setName] = useState('')
-  const [nameError, setNameError] = useState('')
+  const [user, setUser] = useState('')
+  const [value, setValue] = useState('')
 
-  let isError = false
+  const inputRef = useRef(null)
 
-  const handleClick = () => {
-    console.log('log in - click!')
+  useEffect(() => {
+    inputRef.current.focus()
+  }, [])
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (value.trim() === '') {
+      alert('Please, enter your name')
+    }
+    setUser(value)
+    setValue('')
   }
 
-  const handleSubmit = () => {
-    // if (name === "") {
-    //   nameError = "Поле обязательное для заполнения";
-    //   isError = true;
-    // } else if (name.charAt(0) !== state.name.charAt(0).toUpperCase()) {
-    //   nameError = "Имя должно начинаться с большой буквы";
-    //   isError = true;
-    // } else {
-    //   nameError = "";
-    // }
-
-    reset()
-  }
-
-  const reset = () => {
-    setName('')
-    setNameError('')
+  console.log(user)
+  const onChangeInput = (e) => {
+    const { value } = e.target
+    setValue(value)
   }
 
   return (
     <>
-      {/* <div className={s.wrap}>
-        <img src={image} className={s.image} alt='Not found page' />
-      </div> */}
-      <form className={s.form}>
+      <form className={s.form} onSubmit={handleSubmit}>
         <label className={s.label}>
           User Name:
           <input
             className={s.input}
             type='text'
-            // name={(e) => e.target.name}
-            // value={(e) => e.target.value}
+            name='name'
+            value={value}
             placeholder='Example: Kateryna'
-            onChange={handleSubmit}
+            onChange={onChangeInput}
+            ref={inputRef}
           />
         </label>
         <BigButton type='button' text='submit' onClick={handleSubmit} />
