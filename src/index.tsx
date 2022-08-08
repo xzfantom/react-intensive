4e69c1d6d018169d2b9c27c4dae768ac795b36bc
Artsiom Martynau <xzfantom@gmail.com>
2022-08-08 15:47:23 +0400

    basename fix

--- src/index.tsx
+++ src/index.tsx
@@ -15,15 +15,15 @@ const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
-      <Routes>
-            <Route path='/' element={<App />}>
+      <BrowserRouter basename='/react-intensive'>
+        <Routes>
+          <Route path='/' element={<App />}>
             <Route index element={<TodoList />} />
             <Route path='login' element={<Authentication />} />
             <Route path='todoList' element={<TodoList />} />
             <Route path='*' element={<NotFound />} />
           </Route>
-          </Routes>
+        </Routes>
       </BrowserRouter>
     </Provider>
   </React.StrictMode>
